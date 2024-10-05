e1782aef6282c3f31391c2abbfec7b5f8c08d6ff
mennamohamed0207 <menamohamed0207@gmail.com>
2024-10-05 08:05:38 +0300

    buggy: add moisture api

--- backend/venv/server.py
+++ backend/venv/server.py
@@ -10,6 +10,13 @@ from pymongo import MongoClient
 from crontab import CronTab
 from datetime import datetime
 from flask_pymongo import PyMongo
+import h5py 
+from src.download_SPL3SMP import cmr_download, cmr_search
+import math 
+from mpl_toolkits.basemap import Basemap
+import os
+from datetime import datetime, timedelta
+
 
 
 # # Create a new CronTab instance for the current user
@@ -201,6 +208,167 @@ def update_location():
     else:
         return jsonify({"message": "No matching user found to update."}), 404
 
+def get_bounding_box(lat, lon, above_below_km, left_right_km):
+    # Latitude and longitude change calculations
+    lat=float(lat)
+    lon=float(lon)
+    lat_change = above_below_km / 111
+    lon_change = left_right_km / (111 * math.cos(math.radians(lat)))
+    
+    # Bounding box coordinates
+    min_lat = lat - lat_change #S
+    max_lat = lat + lat_change #N
+    min_lon = lon - lon_change #W
+    max_lon = lon + lon_change #E
+    
+    return min_lon, min_lat, max_lon, max_lat
+
+def get_bounding_box_str(lat, lon, above_below_km, left_right_km):
+    min_lon, min_lat, max_lon, max_lat = get_bounding_box(lat, lon, above_below_km, left_right_km)
+    return f"{min_lon},{min_lat},{max_lon},{max_lat}"
+def get_soil_moist_from_path(path, bounding_box, is_am = True):
+    with h5py.File(path, mode="r") as f:
+        field_time = 'AM' if is_am else "PM"
+        name = f'/Soil_Moisture_Retrieval_Data_{field_time}/soil_moisture'
+
+        data = f[name][:]
+        _FillValue = f[name].attrs['_FillValue']
+        valid_max = f[name].attrs['valid_max']
+        valid_min = f[name].attrs['valid_min']        
+        
+        invalid = np.logical_or(data > valid_max,
+                                data < valid_min)
+        invalid = np.logical_or(invalid, data == _FillValue)
+        
+        data[invalid] = 0 # to ignore in getting average
+        # get 
+        latitude = f['/Soil_Moisture_Retrieval_Data_AM/latitude'][:]
+        longitude = f['/Soil_Moisture_Retrieval_Data_AM/longitude'][:]
+        min_lon, min_lat, max_lon, max_lat = bounding_box
+        print(min_lon,min_lat,max_lon,max_lat)
+        
+        n, m = data.shape
+        plot(path,(min_lon,max_lon),(min_lat,max_lat))
+        sum, cnt = 0,0
+        for i in range(n):
+            for j in range(m):
+                if (min_lon <= longitude[i][j] <= max_lon) and (min_lat <= latitude[i][j] <= max_lat) and (data[i][j] != 0):
+                    sum += data[i][j]
+                    cnt += 1
+        return (sum / cnt)
+
+def plot(path, lon_range=None, lat_range=None):
+    """
+    Plots soil moisture data from the given HDF5 file path, filtering by longitude and latitude if provided.
+
+    Parameters:
+        path (str): Path to the HDF5 file.
+        lon_range (tuple, optional): A tuple of (min_longitude, max_longitude) to filter data by longitude.
+        lat_range (tuple, optional): A tuple of (min_latitude, max_latitude) to filter data by latitude.
+    """
+    with h5py.File(path, mode="r") as f:
+        # Load the soil moisture data
+        name = '/Soil_Moisture_Retrieval_Data_AM/soil_moisture'
+        data = f[name][:]
+        
+        # Load metadata attributes
+        units = f[name].attrs['units'].decode('ascii', 'replace')
+        long_name = f[name].attrs['long_name'].decode('ascii', 'replace')
+        _FillValue = f[name].attrs['_FillValue']
+        valid_max = f[name].attrs['valid_max']
+        valid_min = f[name].attrs['valid_min']
+        
+        # Mask invalid data
+        invalid = np.logical_or(data > valid_max, data < valid_min)
+        invalid = np.logical_or(invalid, data == _FillValue)
+        data[invalid] = np.nan
+        data = np.ma.masked_where(np.isnan(data), data)
+        
+        # Get the geolocation data
+        latitude = f['/Soil_Moisture_Retrieval_Data_AM/latitude'][:]
+        longitude = f['/Soil_Moisture_Retrieval_Data_AM/longitude'][:]
+
+        # Filter data based on the provided latitude and longitude ranges
+        if lon_range:
+            lon_mask = (longitude >= lon_range[0]) & (longitude <= lon_range[1])
+        else:
+            lon_mask = np.ones_like(longitude, dtype=bool)  # Include all data if not specified
+        
+        if lat_range:
+            lat_mask = (latitude >= lat_range[0]) & (latitude <= lat_range[1])
+        else:
+            lat_mask = np.ones_like(latitude, dtype=bool)  # Include all data if not specified
+        
+        # Combine masks
+        combined_mask = lon_mask & lat_mask
+
+        # Apply combined mask to filter data and locations
+        filtered_data = data[combined_mask]
+        filtered_longitude = longitude[combined_mask]
+        filtered_latitude = latitude[combined_mask]
+
+        # Plotting using Basemap
+        m = Basemap(projection='cyl', resolution='l',
+                    llcrnrlat=-90, urcrnrlat=90,
+                    llcrnrlon=-180, urcrnrlon=180)
+        m.drawcoastlines(linewidth=0.5)
+        m.drawparallels(np.arange(-90, 91, 45))
+        m.drawmeridians(np.arange(-180, 180, 45), labels=[True, False, False, True])
+        
+        # Plot the filtered soil moisture data
+        scatter = m.scatter(filtered_longitude, filtered_latitude, c=filtered_data, s=1, cmap=plt.cm.jet,
+                            edgecolors=None, linewidth=0)
+        cb = m.colorbar(scatter, location="bottom", pad='10%')
+        cb.set_label(units)
+        
+        # Save plot
+        basename = os.path.basename(path)
+        plt.title(f'{basename}\n{long_name}')
+        plt.savefig(f"../../assets/plots/{basename}.png")
+        plt.show()
+
+    
+def fetch_data(lat, lon, folder_path="datasets/smap", time_start = '2024-10-01T00:00:00Z', time_end = '2024-10-02T21:48:13Z'):
+    short_name = 'SPL3SMP'
+    version = '009'
+    polygon = ''
+    filename_filter = ''
+    url_list = []
+    bounding_box = get_bounding_box_str(lat, lon, 100, 100)
+    force = False
+    quiet = False
+    
+    try:
+        url_list = cmr_search(short_name, version, time_start, time_end,
+                              bounding_box=bounding_box, polygon=polygon,
+                              filename_filter=filename_filter, quiet=quiet)
+        cmr_download(url_list, force=force, quiet=quiet, folder_path=folder_path)
+    except KeyboardInterrupt: # to graceful shutdown when intrrput
+        quit()
+
+    return url_list
+@app.route("/moisture",methods=['POST'])
+def getMoisture():
+    data=request.get_json()
+    lat=data.get("lat")
+    lon=data.get("lon")
+    # Get today's date
+    today = datetime.utcnow()
+
+    # Calculate yesterday's date
+    yesterday = today - timedelta(days=1)
+
+    # Format it in the desired format: YYYY-MM-DDTHH:MM:SSZ
+    yesterday_formatted = yesterday.strftime('%Y-%m-%dT00:00:00Z')
+    url_list = fetch_data(lat, lon)
+    folder_path = "datasets/smap"
+    file_name = url_list[0].split('/')[-1]
+    path = folder_path + "/" + file_name
+    bounding_box = get_bounding_box(lat, lon, 100, 100)
+    answer = get_soil_moist_from_path(path, bounding_box)
+    return jsonify(answer)
+    
+        
 
 if __name__ == '__main__':
     app.run()
